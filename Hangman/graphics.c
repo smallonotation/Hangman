7b34740f9caf13103a67fcc8716d692cf3550c2b
WinzuDarkpaw <niconowak3@gmail.com>
2021-04-12 11:59:22 +0200

    graphics
    Basis-Funktionen hinzugefügt
    Definition + Deklaration
    change_codepage()
    clear_console()
    draw_border(width, height)

--- Hangman/graphics.c
+++ Hangman/graphics.c
@@ -1,7 +1,66 @@
 /**
  *  Diese Quell-Datei dient zur Definition der Grafiken.
  *
- *  @author Jesse Kroeske, Timo Thiede - 07.04.2021
+ *  @author Jesse Kroeske, Timo Thiede, Nico Nowak - 07.04.2021
  **/
 
+//#include <windows.h>
 #include "graphics.h"
+
+void change_codepage()
+{
+	// Ändert die Codepage zu 437 für die Ascii-Zeichen des Rahmens.
+	system("chcp 437");
+}
+
+void clear_console()
+{
+	// Löscht alle Zeichen die sich in der Konsole befinden.
+	system("cls");
+}
+
+int draw_border(int width, int height)
+{
+	// Zählervariablen
+	int y, x;
+
+	// Rahmen erzeugen.
+	// Y-Achse
+	for (y = 0; y < height; y++)
+	{
+		// X-Achse auf der Höhe Y
+		for (x = 0; x < width; x++)
+		{
+			if (y == 0 && x == 0)
+			{
+				printf("\xC9");
+			}
+			else if (y == 0 && x == width - 1)
+			{
+				printf("\xBB");
+			}
+			else if (y == height - 1 && x == 0)
+			{
+				printf("\xC8");
+			}
+			else if (y == height - 1 && x == width - 1)
+			{
+				printf("\xBC");
+			}
+			else if (y == 0 || y == height)
+			{
+				printf("\xCD");
+			}
+			else if (x == 0 || x == width - 1)
+			{
+				printf("\xBA");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+	}
+
+	return 0;
+}
--- Hangman/graphics.h
+++ Hangman/graphics.h
@@ -1,10 +1,27 @@
 /**
  *  Diese Header-Datei dient zur Deklaration der Grafiken.
  *
- *  @author Jesse Kroeske, Timo Thiede - 07.04.2021
+ *  @author Jesse Kroeske, Timo Thiede, Nico Nowak - 07.04.2021
  **/
 
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
+
+/// <summary>
+///		Ändert die Codepage um die Ascii-Zeichen für die Rahmen nutzen zu können.
+/// </summary>
+void change_codepage();
+
+/// <summary>
+///		Löscht alle Zeichen aus der Konsole.
+/// </summary>
+void clear_console();
+
+/// <summary>
+///		Erzeugt einen Rahmen aus den Ascii-Zeichen. (Die obere Linke Ecke startet an der Cursor-Position)
+///		width = Die Breite des Rahmens (muss mindestens 2 sein).
+///		height = Die Höhe des Rahmens (muss mindestens 2 sein).
+/// </summary>
+int draw_border(int width, int height);
 
 #endif // GRAPHICS_H
